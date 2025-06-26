fb5acb4524266bdf5f901d25468aff447158f3ad
shennawy <ahmedshennawy997@gmail.com>
2025-06-27 02:55:24 +0300

    deployment fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   useEffect(() => {
-    // @ts-ignore
+    // @ts-expect-error
     import("bootstrap/dist/js/bootstrap.bundle.min.js")
       .then(() => console.log("Bootstrap loaded"))
       .catch(console.error);
