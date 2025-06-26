fd9dafc22791b126d9b85d329b73361f591125e1
shennawy <ahmedshennawy997@gmail.com>
2025-06-27 02:59:49 +0300

    description

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   useEffect(() => {
-    // @ts-expect-error
+    // @ts-expect-error - no props needed here
     import("bootstrap/dist/js/bootstrap.bundle.min.js")
       .then(() => console.log("Bootstrap loaded"))
       .catch(console.error);
